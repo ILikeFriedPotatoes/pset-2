a19533b2a10b8c2d644bcfc44f74e9a72c2a0a5d
Joseph Wang <jwang@ucvts.tec.nj.us>
2019-09-24 09:03:11 -0400

    Finished exercise 5 and 6

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -28,7 +28,8 @@ public class ProblemSet2 {
          * name, grade, age, and hometown.
          */
 
-        System.out.print("Enter your first name: ");
+        /*
+        System.out.print("\nEnter your first name: ");
         String firstName = in.next();
         System.out.print("Enter your last name: ");
         String lastName = in.next();
@@ -39,10 +40,11 @@ public class ProblemSet2 {
         in.nextLine();
         System.out.print("Enter your hometown: ");
         String hometown = in.nextLine();
-        System.out.println("NAME :" + firstName + " " + lastName );
-        System.out.println("GRADE:" + grade );
-        System.out.println("AGE:" + age );
-        System.out.println("HOMETOWN: " + hometown );
+        System.out.println("\nNAME   : " + firstName + " " + lastName );
+        System.out.println("GRADE    : " + grade );
+        System.out.println("AGE      : " + age );
+        System.out.println("HOMETOWN : " + hometown );
+        */
 
         /*
          * Exercise 2.
@@ -56,22 +58,22 @@ public class ProblemSet2 {
          double dollarAmount = in.nextDouble();
          double valueRemaining = dollarAmount;
          final double DOLLAR_VALUE = 1.00;
-         final double QUARTER_VALUE = 0.25;
-         final double DIME_VALUE = 0.10;
-         final double NICKEL_VALUE = 0.05;
-         final double PENNY_VALUE = 0.01;
          double isThereADollar = Math.floor(dollarAmount / DOLLAR_VALUE);
          valueRemaining %= DOLLAR_VALUE;
-         System.out.printf("DOLLARS  : %.0f \n", isThereADollar);
+         System.out.printf("\nDOLLARS  : %.0f \n", isThereADollar);
+         final double QUARTER_VALUE = 0.25;
          double quarterAmount = Math.floor(valueRemaining / QUARTER_VALUE);
          valueRemaining %= QUARTER_VALUE;
          System.out.printf("QUARTERS : %.0f\n", quarterAmount);
+         final double DIME_VALUE = 0.10;
          double dimeAmount = Math.floor(valueRemaining / DIME_VALUE);
          valueRemaining %= DIME_VALUE;
          System.out.printf("DIMES    : %.0f\n", dimeAmount);
+         final double NICKEL_VALUE = 0.05;
          double nickelAmount = Math.floor(valueRemaining / NICKEL_VALUE);
          valueRemaining %= NICKEL_VALUE;
          System.out.printf("NICKELS  : %.0f\n", nickelAmount);
+         final double PENNY_VALUE = 0.01;
          valueRemaining /= PENNY_VALUE;
          System.out.printf("PENNIES  : %.0f\n", valueRemaining);
          */
@@ -86,12 +88,12 @@ public class ProblemSet2 {
          /*
          System.out.print("\nEnter a dollar amount: ");
          valueRemaining = in.nextDouble();
-         final double FIVE_DOLLAR_VALUE = 5.00;
-         final double TEN_DOLLAR_VALUE = 10.00;
          double billsUsed = 0;
          double coinsUsed = 0;
+         final double TEN_DOLLAR_VALUE = 10.00;
          billsUsed += Math.floor(valueRemaining / TEN_DOLLAR_VALUE);
          valueRemaining %= TEN_DOLLAR_VALUE;
+         final double FIVE_DOLLAR_VALUE = 5.00;
          billsUsed += Math.floor(valueRemaining / FIVE_DOLLAR_VALUE);
          valueRemaining %= FIVE_DOLLAR_VALUE;
          billsUsed += Math.floor(valueRemaining / DOLLAR_VALUE);
@@ -115,23 +117,23 @@ public class ProblemSet2 {
          */
 
          //Delete this variable later
-
+         /*
          System.out.print("\nEnter a number of inches: ");
-         double valueRemaining = in.nextDouble();
+         valueRemaining = in.nextDouble();
          final double INCHES_TO_MILES_CONVERSION = 63360;
-         final double INCHES_TO_YARD_CONVERSION = 36;
-         final double INCHES_TO_FEET_CONVERSION = 12;
          double numberOfMiles = Math.floor(valueRemaining / INCHES_TO_MILES_CONVERSION);
          System.out.printf("\nMILES  : %.0f" , numberOfMiles);
          valueRemaining %= INCHES_TO_MILES_CONVERSION;
+         final double INCHES_TO_YARD_CONVERSION = 36;
          double numberOfYards = Math.floor(valueRemaining / INCHES_TO_YARD_CONVERSION);
          System.out.printf("\nYARDS  : %.0f" , numberOfYards);
          valueRemaining %= INCHES_TO_YARD_CONVERSION;
+         final double INCHES_TO_FEET_CONVERSION = 12;
          double numberOfFeet =  Math.floor(valueRemaining / INCHES_TO_FEET_CONVERSION);
          System.out.printf("\nFEET   : %.0f" ,numberOfFeet);
          valueRemaining %= INCHES_TO_FEET_CONVERSION;
          System.out.printf("\nINCHES : %.0f\n" , valueRemaining);
-
+         */
 
         /*
          * Exercise 5.
@@ -140,7 +142,19 @@ public class ProblemSet2 {
          * meters, and centimeters.
          */
 
-
+         /*
+         System.out.print("\nEnter a number of centimeters: ");
+         double valueRemaining = in.nextDouble();
+         final double CENTIMETER_TO_KILOMETERS_CONVERSION = 100000;
+         double kilometers = Math.floor(valueRemaining / CENTIMETER_TO_KILOMETERS_CONVERSION);
+         System.out.printf("\nKILOMETERS  : %.0f", kilometers);
+         valueRemaining %= CENTIMETER_TO_KILOMETERS_CONVERSION;
+         final double CENTIMETER_TO_METERS_CONVERSION = 100;
+         double meters = Math.floor(valueRemaining / CENTIMETER_TO_METERS_CONVERSION);
+         System.out.printf("\nMETERS      : %.0f", meters);
+         valueRemaining %= CENTIMETER_TO_METERS_CONVERSION;
+         System.out.printf("\nCENTIMETERS : %.0f\n", valueRemaining\n);
+         */
 
         /*
          * Exercise 6.
@@ -148,7 +162,12 @@ public class ProblemSet2 {
          * Given a diameter, print the area and circumference of the corresponding circle.
          */
 
-
+         System.out.print("\nEnter a diameter: ");
+         double diameter = in.nextDouble();
+         double area = Math.PI * Math.pow(diameter / 2, 2);
+         double circumference = Math.PI * diameter;
+         System.out.printf("\nAREA          : %.2f", area);
+         System.out.printf("\nCIRCUMFERENCE : %.2f\n", circumference);
 
         /*
          * Exercise 7.
@@ -157,7 +176,7 @@ public class ProblemSet2 {
          * corresponding rectangle.
          */
 
-
+         
 
         /*
          * Exercise 8.
